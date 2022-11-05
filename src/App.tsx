111f8435b56ee6588787ed2bfa78a74e1363073a
rifxtt <dc.wang547@gmail.com>
2022-11-04 21:52:03 -0400

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+
+  //useState hooks
+  const [name, setName] = useState('Dylan');
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{name}</p>
+      <button>Change name</button>
       </header>
     </div>
   );
