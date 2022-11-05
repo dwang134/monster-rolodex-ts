0c19610e275bdb508be62039842698793d7a368c
rifxtt <dc.wang547@gmail.com>
2022-11-04 22:49:33 -0400

    onclick state funct

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App:React.FC= () =>  {
   
 
   //useState hooks
-  const [name, setName] = useState('Dylan');
+  const [name, setName] = useState<string>('Dylan Wang');
+  const [company, setComapny] = useState<string>('ZTM');
+  
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{name}</p>
-      <button>Change name</button>
+        <p>My name is {name}, and I work at {company}</p>
+      <button onClick={()=> setName('Name Changed')}>Change name</button>
       </header>
     </div>
   );
