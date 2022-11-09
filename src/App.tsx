84b243b3443a711e61bf7d408ef2e600f17e3291
rifxtt <dc.wang547@gmail.com>
2022-11-09 10:41:14 -0500

    input onChange

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,10 @@ import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {User} from '../types/Monster'
+import { isConstructorDeclaration } from 'typescript';
 
 const App:React.FC= () =>  {
   
-
-  //useState hooks
-  // const [monster1, setMonster1] = useState<Monster>({name: "Monster 1"});
-  // const [monster2, setMonster2] = useState<Monster>({name: "Monster 2"});
-  // const [monster3, setMonster3] = useState<Monster>({name: "Monster 3"});
-
-    //monster object
     const monsterArray= [
       {
         name: 'Monster 1'
@@ -27,6 +21,7 @@ const App:React.FC= () =>  {
   const [monsters, setMonsters] = useState<User []>([]);
 
     useEffect(()=> {
+      console.log("UseEffect is called");
       getUserData();
     }, [])
 
@@ -34,29 +29,21 @@ const App:React.FC= () =>  {
     const getUserData = async() => {
       const res = await fetch('https://jsonplaceholder.typicode.com/users');
       const data = await res.json();
+      console.log(data);
       setMonsters(data);
     }
 
-
-    // componentDidMount(
-    //   const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    //   const data = res.json();
-      
-    // );
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <input className= 'search-box' type= 'search' placeholder= 'search monsters' onChange={(e)=> {console.log(e.target.value)}}/>
         <div>
         {monsters.map((monster, index)=> {
           return <p key= {monster.name}>{monster.name}</p>
         })}
         </div>
       {/* <button onClick={()=> setMonster1({name: 'Name Changed'})}>Change name</button> */}
-      </header>
     </div>
-  );
+  )
 }
 
 export default App;
